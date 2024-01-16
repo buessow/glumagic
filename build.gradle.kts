7b53f04748cc47372a99e8892847af3cefc5789a
Robert Buessow <buessow@gmail.com>
2024-01-16 20:39:57 +0100

    Build fatjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 
-dependencies {
-//  implementation("com.android.library:com.android.library.gradle.plugin:8.1.4")
-
-}
 plugins {
   id("org.jetbrains.kotlin.android")  version "2.0.0-Beta2" apply false
 }
