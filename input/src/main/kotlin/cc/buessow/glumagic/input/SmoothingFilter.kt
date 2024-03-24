d192fb7552c89f21fe333b0d6dfc09c19cd86d6d
Robert Buessow <buessow@gmail.com>
2024-03-24 10:33:23 +0100

    Fix smoothing with non-finite values

--- input/src/main/kotlin/cc/buessow/glumagic/input/SmoothingFilter.kt
+++ input/src/main/kotlin/cc/buessow/glumagic/input/SmoothingFilter.kt
@@ -16,9 +16,11 @@ class SmoothingFilter(filterName: String, filterParams: Map<String, Any>) {
       }
     }
 
+  private val windowSize = getInt(filterParams, "windowSize", 3)
+
   private val kernelFilter: _KernelFilter? = when (filterName) {
     "none" -> null
-    "median" -> Median(getInt(filterParams, "windowSize", 3))
+    "median" -> Median(windowSize)
     "savgol" -> Savgol(
         getInt(filterParams, "windowSize", 3),
         getInt(filterParams, "polynomialOrder", 2))
@@ -26,6 +28,24 @@ class SmoothingFilter(filterName: String, filterParams: Map<String, Any>) {
     else -> throw IllegalArgumentException("unknown smoothing filter '$filterName'")
   }
 
-  fun filter(values: List<Double>) =
-    kernelFilter?.filter(values.toDoubleArray())?.toList() ?: values
+  fun filter(values: List<Double>): List<Double> {
+    if (kernelFilter == null) return values
+
+    // Make sure we don't pass nan or infinity to the filter, since it won't terminate.
+    // We split the input into sub-lists of finite values and pass them individually to the
+    // filter if they are long enough. Other values are simply copied over.
+    val result = mutableListOf<Double>()
+    var pos = 0
+    while (result.size < values.size) {
+      val start = pos
+      while (pos < values.size && values[pos].isFinite()) pos++
+      if (pos - start >= windowSize) {
+        result.addAll(kernelFilter.filter(values.subList(start, pos).toDoubleArray()).asList())
+      } else {
+        result.addAll(values.subList(start, pos))
+      }
+      while (pos < values.size && !values[pos].isFinite()) result.add(values[pos++])
+    }
+    return result
+  }
 }
--- input/src/test/kotlin/cc/buessow/glumagic/input/SmoothingFilterTest.kt
+++ input/src/test/kotlin/cc/buessow/glumagic/input/SmoothingFilterTest.kt
@@ -17,4 +17,29 @@ class SmoothingFilterTest {
         listOf(3.114, 4.143, 4.886, 3.257, 5.429, 10.743),
         eps=1e-2)?.also { fail(it) }
   }
+
+  @Test
+  fun savgol1() {
+    val filter = SmoothingFilter("savgol", mapOf("windowSize" to 7, "polynomialOrder" to 2))
+    ArrayApproxCompare.getMismatch(
+        filter.filter(List(10) {0.0}),
+        List(10){0.0},
+        eps=1e-6)
+    val l = MutableList(10) { 0.0 }
+    l[0] = Double.NaN
+    ArrayApproxCompare.getMismatch(filter.filter(l), l, eps=1e-6)
+    l[1] = Double.NaN
+    ArrayApproxCompare.getMismatch(filter.filter(l), l, eps=1e-6)
+    val l1 = MutableList(10) { 0.0 }
+    l1[4] = Double.NaN
+    ArrayApproxCompare.getMismatch(filter.filter(l1), l1, eps=1e-6)
+    val l2 = MutableList(10) { 0.0 }
+    l2[4] = Double.NaN
+    ArrayApproxCompare.getMismatch(filter.filter(l2), l2, eps=1e-6)
+    val l3 = MutableList(10) { 0.0 }
+    l3[l3.size-1] = Double.NaN
+    ArrayApproxCompare.getMismatch(filter.filter(l3), l3, eps=1e-6)
+    val l4 = MutableList(10) { Double.NEGATIVE_INFINITY }
+    ArrayApproxCompare.getMismatch(filter.filter(l4), l4, eps=1e-6)
+  }
 }
